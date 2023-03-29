84ea3471a440a6db8f7bcb08b829b653449b471d
Diego Feldman <diegofeldman@gmail.com>
2023-03-29 17:46:48 -0300

    Tercer Commit

--- src/encuentro9/Enc09Ej06.java
+++ src/encuentro9/Enc09Ej06.java
@@ -18,6 +18,7 @@ public class Enc09Ej06 {
      */
     public static void main(String[] args) {
         Scanner leer = new Scanner (System.in);
+        System.out.println("Ingrese un número");
         
     }
     
